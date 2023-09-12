a66aff62b7496af9cf3434401500f980e3962a03
dande100 <vasanthid10@gmail.com>
2023-09-11 19:04:15 -0500

    api's added

--- src/app.py
+++ src/app.py
@@ -15,6 +15,28 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+jackson_family.add_member({
+    "id": jackson_family._generateId(),
+    "age": 35,
+    "first_name": 'Jane',
+    "last_name": jackson_family.last_name,
+    "lucky_numbers": [10, 14, 3]
+})
+jackson_family.add_member({
+    "id": jackson_family._generateId(),
+    "first_name": 'John',
+    "age": 33,
+    "last_name": jackson_family.last_name,
+    "lucky_numbers": [7, 13, 22]
+})
+jackson_family.add_member({
+    "id": jackson_family._generateId(),
+    "first_name": 'Jimmy',
+    "age": 5,
+    "last_name": jackson_family.last_name,
+    "lucky_numbers": [1]
+})
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -26,17 +48,27 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_single_family_member(member_id):
+    get_member = jackson_family.get_member(member_id)
+    if get_member is not None:
+      return jsonify(get_member), 200
+    return jsonify({"error": "Member not found"}), 404
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def create_member():
+    member = request.get_json(force=True)
+    member_obj = jackson_family.add_member(member)
+    return jsonify(member_obj), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    members = jackson_family.delete_member(member_id)
+    return jsonify(members), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
